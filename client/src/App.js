50709487c35ec93bf3fe6f748c59b9de96094c71
Giwon <leecode99@gmail.com>
2023-01-17 15:26:24 -0500

    CRUD, take data from front end to back end

---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+
+import React, { useState, useEffect } from "react";
+import './App.css';
+import Axios from 'axios'
+
+
+
+function App() {
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+  const [movieReviewList, setMovieReviewList] = useState([]);
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
+
+      setMovieReviewList(response.data);
+    });
+  }, []);//call only once
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      movieName: movieName,
+      movieReview: review,
+    }).then(() => {
+      alert("success insert");
+    })
+  };
+
+  return (
+    <>
+      <h1>CRUP practice</h1>
+      <div className="form">
+        <label>Movie Name</label>
+        <input type="text" name="movieName" onChange={(e) => setMovieName(e.target.value)}></input>
+        <label>Review</label>
+        <input type="text" name="review" onChange={(e) => setReview(e.target.value)}></input>
+        <button onClick={submitReview}> Sutmit</button>
+      </div>
+
+      {movieReviewList.map((val) => {
+        return <h1 key={val.id}>Movie Name: {val.movie_name} | Movie review: {val.movie_review} </h1>
+      })}
+
+    </>
+
+
+  );
+}
+
+export default App;
+
+// npm i axios: install axios that allows api request to 
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const cors = require('cors'); // npm install cors
+// access mysql
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "CRUDapp",
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());//middle ware, needs when requesting information from frontend to backend
+
+
+app.get('/api/get',(req,res)=>{
+    const sqlSelect = "SELECT * FROM movie_review";
+    db.query(sqlSelect, (err,result)=>{
+        console.log(result);
+        res.send(result);
+    });
+});
+
+app.post('/api/insert', (req, res) => {
+
+    const movieName = req.body.movieName;
+    const movieReivew = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_review (movie_name, movie_review) VALUES (?,?);"
+    db.query(sqlInsert, [movieName, movieReivew], (err, result) => {
+        console.log(err);
+    })
+})
+
+
+app.get("/", (req, res) => {
+    // const sqlInsert = "INSERT INTO movie_review (movie_name, movie_review) VALUES ('inception','good movie');"
+    // db.query(sqlInsert, (err, result) => {
+    //     console.log(err);
+    //     res.send("Hello Worldlasefasefse");
+    // });
+
+});
+
+app.listen(3001, () => {
+    console.log("running on port 3001");
+});
